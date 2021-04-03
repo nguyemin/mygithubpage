4693ded3ac8a76ef7543d2022e896488ad7eed2b
Minh Nguyen <minh.nguyen@maibornwolff.de>
2021-04-03 18:36:30 +0200

    init image gallerey

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "65eb84c64eebd1fd758c";
+/******/ 	var hotCurrentHash = "1e5431a16a3ffaff2eca";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
